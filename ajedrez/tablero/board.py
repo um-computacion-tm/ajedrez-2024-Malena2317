aa614598d3a7d554c9b131583c76d77310967ee0
Malena2317 <me.navarro@alumno.um.edu.ar>
2024-09-06 19:34:15 -0300

    subiendo modificacion del codigo board

--- ajedrez/tablero/board.py
+++ ajedrez/tablero/board.py
@@ -3,7 +3,6 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from piezas.rook import Rook
 
-
 class Board:
     def __init__(self):
         self.__positions__ = []
