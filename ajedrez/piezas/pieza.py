8d3cc765c1ce1a3420685cdfe3e0d6f149e9d658
Malena2317 <me.navarro@alumno.um.edu.ar>
2024-09-30 21:40:24 -0300

    subiendo de nuevo

--- ajedrez/piezas/pieza.py
+++ ajedrez/piezas/pieza.py
@@ -4,41 +4,35 @@ class Piece:
         self.__row__ = row
         self.__col__ = col
         self.__color__ = color
-        
-    def __str__(self):
-            if self.__color__ == "WHITE":
-                return self.white_str
-            else:
-                return self.black_str
 
-    def move(self, to_position, board):
-        # Utiliza la lógica de la clase Position para moverse
-        return self.position.move(to_position, board, self.color)
-        
-class Position:
-    def __init__(self, row, col):
-        # Inicializa una posición en el tablero de ajedrez
-        self.__row__ = row
-        self.__col__ = col
+    def get_coordinates(self):
+        return (self.__row__, self.__col__)
 
-    def is_within_board(self):
-        # Verifica si la posición está dentro del tablero
-        return 0 <= self.row < 8 and 0 <= self.col < 8
-    
-    def can_move_to(self, to_pos, board, color):
+    def update_coordinates(self, new_row, new_col):
+        self.__row__ = new_row
+        self.__col__ = new_col
+
+    def get_color(self):
+        return self.__color__
+
+    def is_within_board(self, to_row, to_col):
+        return 0 <= to_row < 8 and 0 <= to_col < 8
+
+    def can_move_to(self, to_row, to_col, board):
         # Verifica si se puede mover a la nueva posición
-        return (board[to_pos.row][to_pos.col] is None or
-                board[to_pos.row][to_pos.col].color != color)
-
-    def move(self, start_pos, to_pos, board):
-        # Intenta mover la pieza de start_pos a to_pos en el tablero
-        if not to_pos.is_within_board():
+        if not self.is_within_board(to_row, to_col):
             print("Te saliste del tablero!!")
             return False
-        if self.can_move_to(to_pos, board, color):
-            self.row = to_pos.row
-            self.col = to_pos.col
+        if board[to_row][to_col] is None or board[to_row][to_col].get_color() != self.get_color():
             return True
         return False
 
-    
+    def move(self, to_row, to_col, board):
+        if self.can_move_to(to_row, to_col, board):
+            self.update_coordinates(to_row, to_col)
+            board[to_row][to_col] = self
+            return True
+        return False
+
+    def is_valid_move(self, to_row, to_col, board):
+        raise NotImplementedError("Este método debe ser implementado por las subclases") 
