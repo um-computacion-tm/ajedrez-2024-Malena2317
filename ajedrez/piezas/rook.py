1001c94a5afe10ef5cf9a5b4dd4b898ab61c25d9
Malena2317 <me.navarro@alumno.um.edu.ar>
2024-10-14 11:50:17 -0300

    subiendo mas test a rook

--- ajedrez/piezas/rook.py
+++ ajedrez/piezas/rook.py
@@ -4,22 +4,39 @@ from piezas.pieza import Piece
 
 
 
+
 class Rook(Piece):
 
-    def __init__(self, color):
-        self.__color__ = color
+    def __init__(self,row, col, color):
+        super().__init__(row, col, color)
         self.__symbol__ = "♜" if color == "WHITE"  else "♖"
 
     def move(self, to_row, to_col, board):
         if self.is_valid_move(to_row, to_col, board):
-            return super().move(to_row, to_col, board)
+            current_row, current_col = self.get_coordinates()
+            board[current_row][current_col] = None
+            self.update_coordinates(to_row, to_col)
+            board[to_row][to_col] = self
+            return True
         return False
         
+        
     def is_valid_move(self, to_row, to_col, board):
+        # Verificar si la posición está dentro del tablero
+        if not self.is_within_board(to_row, to_col):
+            return False
+
         current_row, current_col = self.get_coordinates()
+        # Verificar movimiento en línea recta
         if current_row == to_row or current_col == to_col:
-            return self._is_path_clear(current_row, current_col, to_row, to_col, board)
+            # Verificar si el camino está libre
+            if self._is_path_clear(current_row, current_col, to_row, to_col, board):
+                # Verificar si la posición de destino está vacía o tiene una pieza de otro color
+                destination_piece = board[to_row][to_col]
+                if destination_piece is None or destination_piece.get_color() != self.get_color():
+                    return True
         return False
+        
 
     def _is_path_clear(self, from_row, from_col, to_row, to_col, board):
         # Verifica si hay obstaculos en el camino
