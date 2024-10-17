1243e8441e895aa55480c529105b5e58a9a66fe9
Malena2317 <163948163+Malena2317@users.noreply.github.com>
2024-10-16 23:26:18 -0300

    Update bishop.py

--- ajedrez/piezas/bishop.py
+++ ajedrez/piezas/bishop.py
@@ -13,3 +13,16 @@ class Alfil(Piece):
         row_diff = abs(to_row - self.get_coordinates()[0])
         col_diff = abs(to_col - self.get_coordinates()[1])
         return row_diff == col_diff and self.can_move_to(to_row, to_col, board)
+
+    def _is_path_clear(self, to_row, to_col, board):
+        # Checks if the diagonal to the destination position is free of obstacles.
+         current_row, current_col = self.get_coordinates()
+         step_row = 1 if to_row > current_row else -1
+         step_col = 1 if to_col > current_col else -1
+          
+          while (current_row, current_col) != (to_row - step_row, to_col - step_col):
+                current_row += step_row
+                current_col += step_col
+                if board.get_piece(current_row, current_col) is not None:
+                      return False
+                 return True
